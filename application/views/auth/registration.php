f44ebeaebd4f2039d464a28ec65b34f817804cca
NuubS <77597376+NuubbS@users.noreply.github.com>
2021-03-27 13:33:48 +0700

    hamdan

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Login &mdash; SIPEKA AKB</title>
+    <title>Register &mdash; Stisla</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/bootstrap/css/bootstrap.min.css">
@@ -16,6 +16,7 @@
     <!-- Template CSS -->
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/components.css">
+    "<?= base_url()?>assets/css/components.css">
     <!-- Start GA -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
     <script>
@@ -37,47 +38,106 @@
             <div class="container mt-5">
                 <div class="row">
                     <div
-                        class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                        class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
                         <div class="login-brand">
-                            <h3>LOGIN SIPEKA AKB</h3>
+                            <img src="<?= base_url()?>assets/img/stisla-fill.svg" alt="logo" width="100"
+                                class="shadow-light rounded-circle">
                         </div>
 
                         <div class="card card-primary">
+                            <div class="card-header">
+                                <h4>Register</h4>
+                            </div>
+
                             <div class="card-body">
-                                <form method="POST" action="#">
+                                <form method="POST">
+                                    <div class="row">
+                                        <div class="form-group col-6">
+                                            <label for="frist_name">First Name</label>
+                                            <input id="frist_name" type="text" class="form-control" name="frist_name"
+                                                autofocus>
+                                        </div>
+                                        <div class="form-group col-6">
+                                            <label for="last_name">Last Name</label>
+                                            <input id="last_name" type="text" class="form-control" name="last_name">
+                                        </div>
+                                    </div>
+
                                     <div class="form-group">
                                         <label for="email">Email</label>
-                                        <input id="email" type="text" class="form-control" name="email"
-                                            placeholder="Enter Email Address" tabindex="1" required autofocus>
+                                        <input id="email" type="email" class="form-control" name="email">
+                                        <div class="invalid-feedback">
+                                        </div>
                                     </div>
 
-                                    <div class="form-group">
-                                        <div class="d-block">
-                                            <label for="password" class="control-label">Password</label>
-                                            <div class="float-right">
-                                                <a href="<?= base_url() ?>dist/auth_forgot_password" class="text-small">
-                                                    Forgot Password?
-                                                </a>
+                                    <div class="row">
+                                        <div class="form-group col-6">
+                                            <label for="password" class="d-block">Password</label>
+                                            <input id="password" type="password" class="form-control pwstrength"
+                                                data-indicator="pwindicator" name="password">
+                                            <div id="pwindicator" class="pwindicator">
+                                                <div class="bar"></div>
+                                                <div class="label"></div>
                                             </div>
                                         </div>
-                                        <input id="password" type="password" class="form-control" name="password"
-                                            placeholder="Password" tabindex="2" required>
+                                        <div class="form-group col-6">
+                                            <label for="password2" class="d-block">Password Confirmation</label>
+                                            <input id="password2" type="password" class="form-control"
+                                                name="password-confirm">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-divider">
+                                        Your Home
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-6">
+                                            <label>Country</label>
+                                            <select class="form-control selectric">
+                                                <option>Indonesia</option>
+                                                <option>Palestine</option>
+                                                <option>Syria</option>
+                                                <option>Malaysia</option>
+                                                <option>Thailand</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group col-6">
+                                            <label>Province</label>
+                                            <select class="form-control selectric">
+                                                <option>West Java</option>
+                                                <option>East Java</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-6">
+                                            <label>City</label>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                        <div class="form-group col-6">
+                                            <label>Postal Code</label>
+                                            <input type="text" class="form-control">
+                                        </div>
                                     </div>
 
                                     <div class="form-group">
-                                        <button type="submit"<?= base_url('pages/dashboard'); ?>"
-                                            class="btn btn-primary btn-lg btn-block" tabindex="4">
-                                            Login
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" name="agree" class="custom-control-input" id="agree">
+                                            <label class="custom-control-label" for="agree">I agree with the terms and
+                                                conditions</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary btn-lg btn-block">
+                                            Register
                                         </button>
-                                        <!-- <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
-											Login
-										</button> -->
                                     </div>
                                 </form>
                             </div>
                         </div>
                         <div class="simple-footer">
-                            Copyright &copy; NuubbS <?= date('Y'); ?>
+                            Copyright &copy; Stisla 2018
                         </div>
                     </div>
                 </div>
