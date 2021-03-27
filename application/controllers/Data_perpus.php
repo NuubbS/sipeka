7d84d4c0dfbe7267696a5cd83b09ffe8b0ef2326
NuubS <77597376+NuubbS@users.noreply.github.com>
2021-03-27 14:01:26 +0700

    hamdan

--- application/controllers/Data_perpus.php
+++ application/controllers/Data_perpus.php
@@ -3,8 +3,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Data_perpus extends CI_Controller
 {
-	public function data_perpus()
+	public function rak()
 	{
-		$this->data_perpus->load('buku');
+		$this->template->load('template','data_perpus/buku');
 	}
 }
--- application/views/data_perpus/rak.php
+++ application/views/data_perpus/rak.php
@@ -4,11 +4,62 @@
         <div class="section-header">
             <h1>Rak</h1>
         </div>
-        <!-- pintasan carousel -->
-        
-        <!-- pintasan carousel -->
-        <!-- data tabel -->
-        <!-- data tabel -->
+
+        <div class="section-body">
+            <p class="section-lead">
+                <a href="#" class="btn btn-icon icon-left btn-primary" data-toggle="modal" data-target="#tambah_user">
+                    <i class="fas fa-user-plus"></i>
+                    Tambahkan
+                    Pengguna
+                </a>
+            </p>
+
+            <!-- animasi table -->
+            <div class="animate__animated animate__fadeInUp animate__fast">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card table-responsive">
+                            <div class="card-body">
+                                <table id="table_user" class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Nama</th>
+                                            <th scope="col">Email</th>
+                                            <th scope="col">Role</th>
+                                            <th scope="col">Status</th>
+                                            <th scope="col">Tgl dibuat</th>
+                                            <th scope="col">Tgl Diubah</th>
+                                            <th scope="col">Tools</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </div>
 <!-- main content -->
+<!-- modal -->
+<div class="modal fade" tabindex="-1" role="dialog" id="tambah_user" aria-hidden="true" style="display: none;">
+    <div class="modal-dialog modal-md" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Modal Title</h5> <button type="button" class="close" data-dismiss="modal"
+                    aria-label="Close"> <span aria-hidden="true">×</span> </button>
+            </div>
+            <div class="modal-body"> Modal body text goes here.</div>
+            <div class="modal-footer bg-whitesmoke br">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- modal -->
